0b632aa985c9c81dff15007fc21beecdd791f6a7
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-03 01:55:22 +0530

    TC2.1 -- returning max float number at 1st position

--- src/com/maxnumber/GenericsTest.java
+++ src/com/maxnumber/GenericsTest.java
@@ -4,23 +4,23 @@ import java.util.*;
 
 public class GenericsTest {
 	public static void main(String[] args) {
-		// User input for Integer numbers
+		// User input for Float numbers
 		Scanner scan = new Scanner(System.in);
 		System.out.print(" Enter the first value: ");
-		Integer first = scan.nextInt();
+		Float first = scan.nextFloat();
 
 		System.out.print(" Enter the second value: ");
-		Integer second = scan.nextInt();
+		Float second = scan.nextFloat();
 
 		System.out.print(" Enter the third value: ");
-		Integer third = scan.nextInt();
+		Float third = scan.nextFloat();
 		GenMax(first, second, third);
 		scan.close();
 	}
 
-	public static Integer GenMax(Integer first, Integer second, Integer third) {
+	public static Float GenMax(Float first, Float second, Float third) {
 
-		Integer max = first; // considering first is max and assigning max to first
+		Float max = first; // considering first is max and assigning max to first
 		if (second.compareTo(max) > 0) {
 			max = second; // comparing with first max and if greater that assigning to max
 		}
@@ -32,8 +32,8 @@ public class GenericsTest {
 	}
 
 	// displaying numbers
-	public static void displayMax(Integer first, Integer second, Integer third, Integer max) {
-		System.out.println("-----To Find Maximum number------");
+	public static void displayMax(Float first, Float second, Float third, Float max) {
+		System.out.println("***********To Find Maximum number*********");
 		System.out.println("The Given Values are:- \na = " + first + "\nb = " + second + "\nc = " + third);
 		System.out.println("The maximum number is : " + max);
 	}
