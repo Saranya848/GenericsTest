ba1506227d6a6f8d2dea2c8f1404a446edf707f8
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-03 01:45:00 +0530

    TC1.2 -- Displaying max number at 2nd position

--- src/com/maxnumber/GenericsTest.java
+++ src/com/maxnumber/GenericsTest.java
@@ -33,7 +33,7 @@ public class GenericsTest {
 
 	// displaying numbers
 	public static void displayMax(Integer first, Integer second, Integer third, Integer max) {
-		System.out.println("***********To Find Maximum number*********");
+		System.out.println("***To Find Maximum number***");
 		System.out.println("The Given Values are:- \na = " + first + "\nb = " + second + "\nc = " + third);
 		System.out.println("The maximum number is : " + max);
 	}
