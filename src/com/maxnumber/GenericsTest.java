0748cf26710043705508a554f635adfa5ead92f6
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-03 02:06:53 +0530

    TC3.2 -- returning max String value at 2nd position

--- src/com/maxnumber/GenericsTest.java
+++ src/com/maxnumber/GenericsTest.java
@@ -3,21 +3,6 @@ package com.maxnumber;
 import java.util.*;
 
 public class GenericsTest {
-	public static void main(String[] args) {
-		// User input for String numbers
-		Scanner scan = new Scanner(System.in);
-		System.out.print(" Enter the first String value: ");
-		String first = scan.nextLine();
-
-		System.out.print(" Enter the second String value: ");
-		String second = scan.nextLine();
-
-		System.out.print(" Enter the third String value: ");
-		String third = scan.nextLine();
-		GenMax(first, second, third);
-		scan.close();
-	}
-
 	public static String GenMax(String first, String second, String third) {
 
 		String max = first; // considering first is max and assigning max to first
@@ -36,6 +21,21 @@ public class GenericsTest {
 		System.out.println("*****To Find Maximum number******");
 		System.out.println("The Given Values are:- \na = " + first + "\nb = " + second + "\nc = " + third);
 		System.out.println("The maximum number is : " + max);
+	}
+
+	public static void main(String[] args) {
+		// User input for String numbers
+		Scanner scan = new Scanner(System.in);
+		System.out.print(" Enter the first String value: ");
+		String first = scan.nextLine();
+
+		System.out.print(" Enter the second String value: ");
+		String second = scan.nextLine();
+
+		System.out.print(" Enter the third String value: ");
+		String third = scan.nextLine();
+		GenMax(first, second, third);
+		scan.close();
 	}
 
 }
