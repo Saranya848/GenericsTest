c1f6c2c8f5b8ec0ae2b0dd9265cd472f89f68e81
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-03 01:57:47 +0530

    TC2.3 -- returning max float number at 3rd position

--- src/com/maxnumber/GenericsTest.java
+++ src/com/maxnumber/GenericsTest.java
@@ -3,20 +3,6 @@ package com.maxnumber;
 import java.util.*;
 
 public class GenericsTest {
-	public static void main(String[] args) {
-		// User input for Float numbers
-		Scanner scan = new Scanner(System.in);
-		System.out.print(" Enter the first value: ");
-		Float first = scan.nextFloat();
-
-		System.out.print(" Enter the second value: ");
-		Float second = scan.nextFloat();
-
-		System.out.print(" Enter the third value: ");
-		Float third = scan.nextFloat();
-		GenMax(first, second, third);
-		scan.close();
-	}
 
 	public static Float GenMax(Float first, Float second, Float third) {
 
@@ -33,9 +19,24 @@ public class GenericsTest {
 
 	// displaying numbers
 	public static void displayMax(Float first, Float second, Float third, Float max) {
-		System.out.println("*****To Find Maximum number******");
+		System.out.println("---To Find Maximum number---");
 		System.out.println("The Given Values are:- \na = " + first + "\nb = " + second + "\nc = " + third);
 		System.out.println("The maximum number is : " + max);
+	}
+
+	public static void main(String[] args) {
+		// User input for Float numbers
+		Scanner scan = new Scanner(System.in);
+		System.out.print(" Enter the first value: ");
+		Float first = scan.nextFloat();
+
+		System.out.print(" Enter the second value: ");
+		Float second = scan.nextFloat();
+
+		System.out.print(" Enter the third value: ");
+		Float third = scan.nextFloat();
+		GenMax(first, second, third);
+		scan.close();
 	}
 
 }
